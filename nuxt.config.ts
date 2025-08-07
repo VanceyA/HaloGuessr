f957538e17413c81cd775edf3235d7ef157091f2
VanceyA <vancevance2003@gmail.com>
2025-08-06 23:15:44 -0600

    Changing favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ]
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     ['@vesp/nuxt-fontawesome', {
